989cc5e67f5644d229e85decacdca7d5f1fa5fc6
Tanner <tsiffren@gmail.com>
2016-02-22 10:04:38 -0700

    second function creates and test the methods of Calculator

--- a3/src/cse360assign3/Calculator.java
+++ a3/src/cse360assign3/Calculator.java
@@ -5,31 +5,41 @@ public class Calculator
 	
 	private int total;
 	
-	public Calculator () {
+	public Calculator () 
+	{
 		total = 0;  // not needed - included for clarity
 	}
 	
-	public int getTotal () {
-		return 0;
+	public int getTotal ()
+	{
+		return total;
 	}
 	
-	public void add (int value) {
-		
+	public void add (int value) 
+	{
+		total = total + value;
 	}
 	
-	public void subtract (int value) {
-		
+	public void subtract (int value) 
+	{
+		total = total - value;
 	}
 	
-	public void multiply (int value) {
-		
+	public void multiply (int value) 
+	{
+		total = total * value;
 	}
 	
-	public void divide (int value) {
-		
+	public void divide (int value) 
+	{
+		if (value == 0)
+		total = 0;
+		else
+		total = total / value;
 	}
 	
-	public String getHistory () {
+	public String getHistory () 
+	{
 		return "";
 	}
 
--- a3/src/cse360assign3/CalculatorTest.java
+++ a3/src/cse360assign3/CalculatorTest.java
@@ -4,43 +4,81 @@ import static org.junit.Assert.*;
 
 import org.junit.Test;
 
-public class CalculatorTest {
+public class CalculatorTest 
+{
 
 	@Test
-	public void testCalculator() {
+	public void testCalculator() 
+	{
 		Calculator data = new Calculator();
 		
 		assertNotNull(data);
 	}
 
 	@Test
-	public void testGetTotal() {
-		fail("Not yet implemented");
+	public void testGetTotal() 
+	{
+		Calculator data = new Calculator();
+		
+		assertEquals(0, data.getTotal());
 	}
 
 	@Test
-	public void testAdd() {
-		fail("Not yet implemented");
+	public void testAdd() 
+	{
+		Calculator data = new Calculator();
+		data.add(4);
+		data.add(2);
+		
+		assertEquals(6, data.getTotal());
 	}
 
 	@Test
-	public void testSubtract() {
-		fail("Not yet implemented");
+	public void testSubtract() 
+	{
+		Calculator data = new Calculator();
+		data.add(4);
+		data.subtract(2);
+		
+		assertEquals(2, data.getTotal());
 	}
 
 	@Test
-	public void testMultiply() {
-		fail("Not yet implemented");
+	public void testMultiply()
+	{
+		Calculator data = new Calculator();
+		data.add(4);
+		data.multiply(2);
+		
+		assertEquals(8, data.getTotal());
 	}
 
 	@Test
-	public void testDivide() {
-		fail("Not yet implemented");
+	public void testDivide() 
+	{
+		Calculator data = new Calculator();
+		data.add(4);
+		data.divide(2);
+		
+		assertEquals(2, data.getTotal());
+	}
+	
+	@Test
+	public void testDivideBY0() 
+	{
+		Calculator data = new Calculator();
+		data.add(4);
+		data.divide(0);
+		
+		assertEquals(0, data.getTotal());
 	}
 
 	@Test
-	public void testGetHistory() {
-		fail("Not yet implemented");
+	public void testGetHistory() 
+	{
+		Calculator data = new Calculator();
+		
+		assertEquals("", data.getHistory());
 	}
 
 }
